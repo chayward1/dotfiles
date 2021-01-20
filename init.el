e353d1f447792eb1956095bf13c424b360eebe94
Christopher James Hayward <chris@chris.xyz>
2021-01-19 20:14:31 -0500

    Enable email

--- init.el
+++ init.el
@@ -249,41 +249,47 @@
          :file-name "daily/%<%Y-%m-%d>"
          :head "#+TITLE: %<%Y-%m-%d>\n")))
 
-;; (use-package mu4e
-;;   :ensure nil
-;;   :config
-;;   (setq mu4e-change-filenames-when-moving t
-;;         mu4e-update-interval (* 5 60) ;; Every 5 minutes.
-;;         mu4e-get-mail-command "mbsync -a"
-;;         mu4e-maildir "~/.cache/mail")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
 
-;;   ;; Ensure plain text scales for all devices.
-;;   (setq mu4e-compose-format-flowed t)
+(use-package mu4e
+  :config
+  (setq mu4e-change-filenames-when-moving t
+        mu4e-update-interval (* 5 60) ;; Every 5 minutes.
+        mu4e-get-mail-command "mbsync -a"
+        mu4e-maildir "~/.cache/mail"
+        mu4e-compose-signature 
+          (concat "Chris Hayward\n"
+                  "https://chrishayward.xyz\n"))
 
-;;   ;; GPG signing key for outbound mail.
-;;   (setq mml-secure-openpgp-signers '("37AB1CB72B741E478CA026D43025DCBD46F81C0F"))
-;;   (add-hook 'message-send-hook 'mml-secure-message-sign-pgpmime)
-      
-;;   ;; Use SMTP to send messages.
-;;   (setq message-send-mail-function 'smtpmail-send-it)
+  ;; Ensure plain text scales for all devices.
+  (setq mu4e-compose-format-flowed t)
 
-;;   ;; Configure mail account(s).
-;;   (setq mu4e-contexts
-;;     (list
-;;       ;; Main
-;;       ;; chris@chrishayward.xyz
-;;       (make-mu4e-context
-;;         :name "Main"
-;;         :match-func
-;;           (lambda (msg)
-;;             (when msg 
-;;               (string-prefix-p "/Main" (mu4e-message-field msg :maildir))))
-;;         :vars
-;;           '((user-full-name . "Christopher James Hayward")
-;;             (user-mail-address . "chris@chrishayward.xyz")
-;;             (smtpmail-smtp-server . "mail.chrishayward.xyz")
-;;             (smtpmail-smtp-service . 587)
-;;             (smtpmail-stream-type . startls))))))
+  ;; GPG signing key for outbound mail.
+  (setq mml-secure-openpgp-signers '("37AB1CB72B741E478CA026D43025DCBD46F81C0F"))
+  (add-hook 'message-send-hook 'mml-secure-message-sign-pgpmime)
+
+  (setq message-send-mail-function 'smtpmail-send-it)  
+
+  ;; Configure mail account(s).
+  (setq mu4e-contexts
+    (list
+      ;; Main
+      ;; chris@chrishayward.xyz
+      (make-mu4e-context
+        :name "Main"
+        :match-func
+          (lambda (msg)
+            (when msg 
+              (string-prefix-p "/Main" (mu4e-message-field msg :maildir))))
+        :vars
+          '((user-full-name . "Christopher James Hayward")
+            (user-mail-address . "chris@chrishayward.xyz")
+            (smtpmail-smtp-server . "mail.chrishayward.xyz")
+            (smtpmail-smtp-service . 587)
+            (smtpmail-stream-type . starttls))))))
+
+(dotfiles/leader
+  "m" '(mu4e :which-key "Mail"))
 
 (setq org-agenda-files '("~/.local/source/brain/daily/"
                          "~/.local/source/secrets/org/"))
@@ -291,7 +297,7 @@
 (dotfiles/leader
   "a" '(org-agenda :which-key "Agenda"))
 
-(use-package ox-hugo
+(use-package ox-hugo 
   :after ox)
 
 (add-to-list 'org-roam-capture-templates
