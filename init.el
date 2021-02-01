158e2fec78d63d517cc1099d757e7d13296202a6
Christopher James Hayward <chris@chrishayward.xyz>
2021-02-01 17:14:41 -0500

    Cleanup

--- init.el
+++ init.el
@@ -1,22 +1,10 @@
+;; Load the host configuration.
 (let ((host-file (concat user-emacs-directory "/hosts/" system-name ".el")))
   (when (file-exists-p host-file)
     (load-file host-file)))
 
+;; Load the enabled modules.
 (dolist (m dotfiles/modules)
   (let ((mod-file (concat dotfiles/home "/modules/" (symbol-name m) ".el")))
     (when (file-exists-p mod-file)
       (load-file mod-file))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("/home/chris/.local/source/secrets/org/birthdays.org" "/home/chris/.local/source/secrets/org/courses.org" "/home/chris/.local/source/secrets/org/holidays.org" "/home/chris/.local/source/dotfiles/daily/2021-02-01.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
