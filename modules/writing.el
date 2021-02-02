280f19c407334c6592dbcc6fd246f434d79ab21b
Christopher James Hayward <chris@chrishayward.xyz>
2021-02-01 19:20:47 -0500

    Reintegrate roam

--- modules/writing.el
+++ modules/writing.el
@@ -1,6 +1,84 @@
 (use-package org-superstar
   :hook (org-mode . org-superstar-mode))
 
+(use-package ox-hugo 
+  :after ox)
+
+(use-package ox-reveal
+  :after ox
+  :custom (org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"))
+
+(use-package org-roam
+  :hook (after-init . org-roam-mode)
+  :custom (org-roam-directory org-directory))
+
+(use-package org-roam-server
+  :hook (org-roam-mode . org-roam-server-mode))
+
+(dotfiles/leader
+  "r" '(:ignore t :which-key "Roam")
+  "rf" '(org-roam-find-file :which-key "Find")
+  "rb" '(org-roam-buffer-toggle-display :which-key "Buffer")
+  "rd" '(:ignore t :which-key "Dailies")
+  "rdd" '(org-roam-dailies-find-date :which-key "Date")
+  "rdt" '(org-roam-dailies-find-today :which-key "Today")
+  "rdm" '(org-roam-dailies-find-tomorrow :which-key "Tomorrow")
+  "rdy" '(org-roam-dailies-find-yesterday :which-key "Yesterday"))
+
+(setq org-roam-capture-templates
+      '(("p" "Posts" plain (function org-roam-capture--get-point)
+          "%?"
+          :file-name "posts/${slug}"
+          :unnarrowed t
+          :head
+"
+#+TITLE: ${title}
+#+AUTHOR: Christopher James Hayward
+#+DATE: %<%Y-%m-%d>
+
+#+ROAM_KEY: https://chrishayward.xyz/posts/${slug}/
+
+#+HUGO_BASE_DIR: ~/.local/source/website
+#+HUGO_AUTO_SET_LASTMOD: t
+#+HUGO_SECTION: posts
+")
+        ("n" "Notes" plain (function org-roam-capture--get-point)
+          "%?"
+          :file-name "notes/${slug}"
+          :unnarrowed t
+          :head
+"
+#+TITLE: ${title}
+#+AUTHOR: Christopher James Hayward
+
+#+ROAM_KEY: https://chrishayward.xyz/notes/${slug}/
+
+#+HUGO_BASE_DIR: ~/.local/source/website
+#+HUGO_AUTO_SET_LASTMOD: t
+#+HUGO_SECTION: notes
+")
+        ("s" "Slides" plain (function org-roam-capture--get-point)
+         "%?"
+         :file-name "slides/${slug}"
+         :unnarrowed t
+         :head
+"
+#+TITLE: ${title}
+#+AUTHOR: Christopher James Hayward
+
+#+REVEAL_ROOT: https://cdn.jsdelivr.net/npm/reveal.js
+")))
+
+(setq org-roam-dailies-capture-templates
+      '(("d" "Default" entry (function org-roam-capture--get-point)
+         "* %?"
+         :file-name "daily/%<%Y-%m-%d>"
+         :head
+"
+#+TITLE: %<%Y-%m-%d>
+#+AUTHOR: Christopher James Hayward
+")))
+
 (use-package mu4e
   :load-path "/usr/share/emacs/site-lisp/mu4e"
   :config
@@ -54,26 +132,6 @@
 (dotfiles/leader
   "a" '(org-agenda :which-key "Agenda"))
 
-(use-package org-journal
-  :config
-  (setq org-journal-dir (concat dotfiles/home "/daily/")
-        org-journal-file-type 'daily
-        org-journal-file-format "%Y-%m-%d.org"
-        org-journal-enable-agenda-integration t
-        org-icalendar-store-UID t
-        org-icalendar-include-todo "all"
-        org-icalendar-combined-agenda-file (concat dotfiles/home "calender.ics")))
-
-(dotfiles/leader
-  "f" '(:ignore t :which-key "Journal")
-  "ff" '(org-journal-new-entry :which-key "New")
-  "fs" '(org-journal-search :which-key "Search")
-  "fl" '(org-journal-open-next-entry :which-key "Next")
-  "fh" '(org-journal-open-previous-entry :which-key "Previous"))
-
-(use-package ox-hugo 
-  :after ox)
-
 (use-package gif-screencast
   :custom
   (gif-screencast-output-directory (concat dotfiles/home "images/")))
@@ -82,7 +140,3 @@
   "s" '(:ignore t :which-key "Screencast")
   "ss" '(gif-screencast-start-or-stop :which-key "Start / Stop")
   "sp" '(gif-screencast-toggle-pause :which-key "Pause"))
-
-(use-package ox-reveal
-  :after ox
-  :custom (org-reveal-root "https://cdn.jsdelivr.net/reveal.js/3.9.2/"))
