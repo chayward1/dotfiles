d67597094043686753546b3e3e218f3e5afe8366
Christopher James Hayward <chris@chrishayward.xyz>
2021-02-03 15:00:19 -0500

    Dashboard upgrades

--- modules/interface.el
+++ modules/interface.el
@@ -47,12 +47,30 @@
         dashboard-set-init-info t
         dashboard-set-file-icons t
         dashboard-set-heading-icons t
+        dashboard-set-navigator t
         dashboard-startup-banner 'logo
         dashboard-projects-backend 'projectile
         dashboard-items '((projects . 5)
                           (recents . 5)
                           (agenda . 5 )))
   (dashboard-setup-startup-hook))
+
+(setq dashboard-navigator-buttons
+      `(;; First row.
+        ((,(all-the-icons-fileicon "brain" :height 1.1 :v-adjust 0.0)
+         "Brain"
+         "Knowledge base"
+         (lambda (&rest _) (browse-url "http://localhost:8080"))))
+        ;; Second row.
+        ((,(all-the-icons-material "public" :height 1.1 :v-adjust 0.0)
+         "Homepage"
+         "Personal website"
+         (lambda (&rest _) (browse-url "https://chrishayward.xyz"))))
+        ;; Third row.
+        ((,(all-the-icons-faicon "university" :height 1.1 :v-adjust 0.0)
+         "Athabasca"
+         "Univeristy login"
+         (lambda (&rest _) (browse-url "https://login.athabascau.ca/cas/login"))))))
 
 (setq initial-buffer-choice
       (lambda ()
