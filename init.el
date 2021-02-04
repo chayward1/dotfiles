066f3d20744c815055c6b1d6652f5c0e38fbb192
Christopher James Hayward <chris@chrishayward.xyz>
2021-02-04 15:16:55 -0500

    Cleanup host pattern

--- init.el
+++ init.el
@@ -1,3 +1,43 @@
+(defvar dotfiles/browser
+  (getenv "BROWSER")
+  "The default browser used by the system.")
+
+(defconst dotfiles/modules-available
+  '(core desktop writing projects interface)
+  "All of the available modules for hosts to load.")
+
+(defvar dotfiles/modules
+  dotfiles/modules-available
+  "Enabled modules, modify this in your host configuration.")
+
+(defvar dotfiles/home
+  user-emacs-directory
+  "Original value of `user-emacs-directory'.")
+
+(defvar dotfiles/cache
+  (expand-file-name "~/.cache/emacs")
+  "Where `user-emacs-directory' will be redirected.")
+
+(defvar dotfiles/idle
+  0.0
+  "Length of time to wait before offering completions.")
+
+(defvar dotfiles/leader-key
+  "SPC"
+  "Custom leader key for custom actions.")
+
+(defvar dotfiles/leader-key-global
+  (concat "C-" dotfiles/leader-key)
+  "Global leader key available everywhere.")
+
+(defvar dotfiles/projects
+  (expand-file-name "~/.local/source/")
+  "Location where source code projects are stored.")
+
+(defvar dotfiles/passwords
+  (expand-file-name "~/.password-store/")
+  "Directory containing the password store.")
+
 ;; Load the host configuration.
 (let ((host-file (concat user-emacs-directory "/hosts/" system-name ".el")))
   (when (file-exists-p host-file)
