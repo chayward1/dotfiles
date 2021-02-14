85eb48823ff118848bcffe4a8c4b54e81d7a20c4
Christopher James Hayward <chris@chrishayward.xyz>
2021-02-13 22:04:06 -0500

    Remove vterm

--- modules/editor.el
+++ modules/editor.el
@@ -62,11 +62,6 @@
 (dotfiles/leader
   "e" '(eshell :which-key "Shell"))
 
-(use-package vterm)
-
-(dotfiles/leader
-  "v" '(vterm-other-window :which-key "Terminal"))
-
 (use-package magit
   :custom (magit-display-buffer-function
            #'magit-display-buffer-same-window-except-diff-v1))
