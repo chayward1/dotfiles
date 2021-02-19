1974eb1955f4ba42f6ba6408658d74d16020344b
Christopher James Hayward <chris@chrishayward.xyz>
2021-02-18 21:02:39 -0500

    Pretty mode

--- modules/interface.el
+++ modules/interface.el
@@ -47,6 +47,9 @@
 (dotfiles/leader
   "tt" '(counsel-load-theme t t :which-key "Theme"))
 
+(use-package pretty-mode
+  :hook (prog-mode . turn-on-pretty-mode))
+
 (when (display-graphic-p)
   (use-package fira-code-mode
     :hook (prog-mode org-mode)))
