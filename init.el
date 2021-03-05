15b5341237332ec93cb96791ec212565bb20227c
Christopher James Hayward <chris@chrishayward.xyz>
2021-03-05 17:30:03 -0500

    Cleanup initialization

--- init.el
+++ init.el
@@ -1,125 +1,44 @@
 ;; Options
 
-;; Here's a complete list of all of the options configurable for each host, and their default values. If a host configuration does not exist, the default values will remain.
-
-;; Configure the system font with a single ~font-family~ and define the size, of which variations to the font size are relative to this value.
-
-
-(defvar dotfiles/font
-  "Fira Code"
-  "Unified system font family, used on all font faces.")
-
-(defvar dotfiles/font-size
-  96
-  "Unified font size, of which all variations are relative to.")
-
-
-
-;; Used by the desktop module to find the appropriate browser.
-
-
-(defvar dotfiles/browser
-  (getenv "BROWSER")
-  "The default browser used by the system.")
-
-
-
-;; Used by the writing module to determine the system language.
-
-
-(defvar dotfiles/language
-  (getenv "LANG")
-  "The default system language.")
-
-
-
-;; All of the available modules defined in the ~dotfiles/modules-available~ constant.
-
-
-(defconst dotfiles/modules-available
-  '(core editor desktop writing projects interface)
-  "All of the available modules for hosts to load.")
-
-
-
-;; Add the modules you want to initialize to the ~dotfiles/modules~ variable.
-
-
-(defvar dotfiles/modules
-  dotfiles/modules-available
-  "Enabled modules, modify this in your host configuration.")
-
-
-
-;; Specify the emacs home, and the cache directory.
-
-
-(defvar dotfiles/home
-  user-emacs-directory
-  "Original value of `user-emacs-directory'.")
-
-
-
-;; Used to seperate the immutable configuration from the stateful package files.
-
-
-(defvar dotfiles/cache
-  (expand-file-name "~/.cache/emacs")
-  "Where `user-emacs-directory' redirects to.")
-
-
-
-;; Functionality like =completion= and =hints= delayed to avoid popups for common manuevers.
-
-
-(defvar dotfiles/idle
-  0.0
-  "Length of time to wait before offering completions.")
-
-
-
-;; Required for the all powerful leader key.
-
-
-(defvar dotfiles/leader-key
-  "SPC"
-  "Custom leader key for custom actions.")
-
-
-
-;; The desktop module requires the global leader key set.
-
-
-(defvar dotfiles/leader-key-global
-  (concat "C-" dotfiles/leader-key)
-  "Global leader key available everywhere.")
-
-
-
-;; Define where the source repositories exist on disk, for integration with the projects module.
-
-
-(defvar dotfiles/projects
-  (expand-file-name "~/.local/source/")
-  "Location where source code projects exist on disk.")
-
-
-
-;; Where the password store exists on disk.
-
-
-(defvar dotfiles/passwords
-  (expand-file-name "~/.password-store/")
-  "Directory containing the password store.")
-
-
-
-;; Configure the public GPG key that Emacs will encrypt files to.
-
-
-(defvar dotfiles/public-key
-  "37AB1CB72B741E478CA026D43025DCBD46F81C0F"
-  "Public PGP key that Emacs will encrypt files to.")
+;; Here's a complete list of all of the options configurable for each host, and their default values. Override any of these configurations in a host file. All of the values are prefixed with ~dotfiles/~. If you need to make configurations to another variable, consider creating a new option. Here are a few examples to get started:
+
+;; + [[file:hosts/localhost.org][Termux]]
+;; + [[file:hosts/raspberry.org][Raspberry]]
+;; + [[file:hosts/acernitro.org][Acernitro]]
+;; + [[file:hosts/virtualbox.org][Virtualbox]]
+
+;; | Name                       | Description                                       |
+;; |----------------------------+---------------------------------------------------|
+;; | dotfiles/font              | Unified system font family                        |
+;; | dotfiles/font-size         | System wide base font size                        |
+;; | dotfiles/browser           | Browser to open URL links                         |
+;; | dotfiles/language          | Dictionary language to load                       |
+;; | dotfiles/modules-p         | Immutable list of all available modules           |
+;; | dotfiles/modules           | Enabled custom modules                            |
+;; | dotfiles/home              | Original value of `user-emacs-directory'          |
+;; | dotfiles/cache             | Redirection target of `user-emacs-directory       |
+;; | dotfiles/idle              | Delay time before offering completions            |
+;; | dotfiles/leader-key        | All powerful keybinding prefix for custom actions |
+;; | dotfiles/leader-key-global | Like the leader-key, but EVERYWHERE!              |
+;; | dotfiles/projects          | Location of source code projects                  |
+;; | dotfiles/passwords         | Location of the system password store             |
+;; | dotfiles/public-key        | Public GPG key to encrypt files for               |
+
+
+(defvar dotfiles/font "Fira Code")
+(defvar dotfiles/font-size 96)
+(defvar dotfiles/browser (getenv "BROWSER"))
+(defvar dotfiles/language (getenv "LANG"))
+(defconst dotfiles/modules-p '(core editor desktop writing projects interface))
+(defvar dotfiles/modules dotfiles/modules-p)
+(defvar dotfiles/home user-emacs-directory)
+(defvar dotfiles/cache (expand-file-name "~/.cache/emacs"))
+(defvar dotfiles/idle 0.0)
+(defvar dotfiles/leader-key "SPC")
+(defvar dotfiles/leader-key-global (concat "C-" dotfiles/leader-key))
+(defvar dotfiles/projects (expand-file-name "~/.local/source/"))
+(defvar dotfiles/passwords (expand-file-name "~/.password-store/"))
+(defvar dotfiles/public-key "37AB1CB72B741E478CA026D43025DCBD46F81C0F")
 
 ;; Startup
 
@@ -132,7 +51,7 @@
 
 
 
-;; Build and load all of the enabled modules.
+;; Breaking down the project into logical units or chapters to keep the code more maintainable and organized. This is also a fundamental requirement to achieve the goal of modularity. 
 
 
 (dolist (m dotfiles/modules)
