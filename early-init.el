63e7c54f592cd624f8a0cbe16feb2a16a4f2dfc0
Christopher James Hayward <chris@chrishayward.xyz>
2021-05-14 18:30:23 -0400

    Move startup code

--- early-init.el
+++ early-init.el
@@ -1,5 +1,5 @@
-(load-file "~/.emacs.d/lisp/cleanup.el")   ;; Separate the immutable from the mutable.
-(load-file "~/.emacs.d/lisp/packages.el")  ;; Get the packages we want, how we want them.
-(load-file "~/.emacs.d/lisp/options.el")   ;; Load option declarations for host configurations.
-(load-file "~/.emacs.d/lisp/hosts.el")     ;; Load your hosts configurations.
-(load-file "~/.emacs.d/lisp/modules.el")   ;; Load your modules definitions.
+(load-file "~/.emacs.d/elisp/cleanup.el")   ;; Separate the immutable from the mutable.
+(load-file "~/.emacs.d/elisp/packages.el")  ;; Get the packages we want, how we want them.
+(load-file "~/.emacs.d/elisp/options.el")   ;; Load option declarations for host configurations.
+(load-file "~/.emacs.d/elisp/hosts.el")     ;; Load your hosts configurations.
+(load-file "~/.emacs.d/elisp/modules.el")   ;; Load your modules definitions.
