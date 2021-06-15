4205b3cbdeaf8c9ed8ecfeba0d3104c8811f7f8b
Christopher James Hayward <chris@chrishayward.xyz>
2021-06-14 20:37:44 -0400

    Delete Emacs init files

--- init.el
+++
@@ -1,8 +0,0 @@
-;; This file is controlled by README.org
-;; Please make any modifications there.
-
-;; Load the host configuration.
-(dotfiles/load-host system-name)
-
-;; Load the enabled modules.
-(dotfiles/load-modules dotfiles/modules)
