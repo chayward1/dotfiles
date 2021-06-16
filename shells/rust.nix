851c6b81a77cb4098f8e85d80bfa1dc43186ee12
Christopher James Hayward <chris@chrishayward.xyz>
2021-06-16 19:26:55 -0400

    Use XDG

---
+++ shells/rust.nix
@@ -0,0 +1,14 @@
+# This file is controlled by /etc/dotfiles/README.org
+{ pkgs ? import <nixpgs> { } }:
+
+with pkgs;
+mkShell {
+  buildInputs = [
+    rustup
+  ];
+  shellHook = ''
+    export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+    export CARGO_HOME="$XDG_DATA_HOME/cargo"
+    export PATH="$CARGO_HOME/bin:$PATH"
+  '';
+}
