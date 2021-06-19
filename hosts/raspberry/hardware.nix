1075f3c14d7b1abd765823b2cee6c6b81af9863a
Christopher James Hayward <chris@chrishayward.xyz>
2021-06-19 09:27:39 -0400

    Fix hardware overlay import path

--- hosts/raspberry/hardware.nix
+++ hosts/raspberry/hardware.nix
@@ -3,7 +3,7 @@
 
 {
   imports = [
-    inputs.nixos-hardware.nixosModules.raspberry-pi.4
+    inputs.nixos-hardware.nixosModules.raspberry-pi-4
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_rpi4;
