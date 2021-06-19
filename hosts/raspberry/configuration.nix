e483d568f9013a27e59542e0398490b0e983ea8f
Christopher James Hayward <chris@chrishayward.xyz>
2021-06-19 11:20:34 -0400

    BUGFIX! Incompabile networkmanager options

--- hosts/raspberry/configuration.nix
+++ hosts/raspberry/configuration.nix
@@ -4,15 +4,9 @@
 {
   networking.hostName = "raspberry";
   networking.firewall.enable = false;
-  networking.wireless.enable = true;
-  networking.wireless.userControlled.enable = true;
   networking.networkmanager.enable = true;
   networking.interfaces.eth0.useDHCP = true;
   networking.interfaces.wlan0.useDHCP = true;
-
-  # Pre-configured wireless networks.
-  networking.wireless.networks.MyWiFi_5C1870.pskRaw =
-    "409b3c85fef1c5737f284d2f82f20dc6023e41804e862d4fa26265ef8193b326";
 
   environment.systemPackages = [
     pkgs.libraspberrypi
