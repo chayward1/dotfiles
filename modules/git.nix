c3ac18a45c976824abffd13fca8320757bb84b8a
Christopher James Hayward <chris@chrishayward.xyz>
2021-06-30 08:47:26 -0400

    Add git-fix shim to fix local repo corruptions

--- modules/git.nix
+++ modules/git.nix
@@ -1,7 +1,20 @@
 # This file is controlled by /etc/dotfiles/README.org
 { pkgs, ... }:
 
-{
+let
+  # Fix any corruptions in the local copy.
+  myGitFix = pkgs.writeShellScriptBin "git-fix" ''
+    if [ -d .git/objects/ ]; then
+      find ./git/objects/ -type f -empty | xargs rm -f
+      git fetch -p
+      git fsck --full
+    fi
+    exit 1
+  '';
+
+in {
+  home.packages = [ myGitFix ];
+
   programs.git = {
     enable = true;
     userName = "Christopher James Hayward";
