5d68e6d30cd1758864f4bd9299a69bf951561e50
Christopher James Hayward <chris@chrishayward.xyz>
2022-03-10 12:21:40 -0500

    Disable networking hosts (retired)

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -10,10 +10,10 @@
   networking.interfaces.ens3.useDHCP = true;
 
   networking.hosts = {
-    "192.168.3.105" = [ "gamingpc" ];
-    "192.168.3.163" = [ "acernitro" ];
-    "192.168.3.182" = [ "raspberry" ];
-    "192.168.3.183" = [ "homecloud" ];
+    # "192.168.3.105" = [ "gamingpc" ];
+    # "192.168.3.163" = [ "acernitro" ];
+    # "192.168.3.182" = [ "raspberry" ];
+    # "192.168.3.183" = [ "homecloud" ];
   };
   users.users.chris.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4wka/LfG3pto15DIm9LIRbb6rWr7/ipCRiCdAKSlY4 chris@chrishayward.xyz"
