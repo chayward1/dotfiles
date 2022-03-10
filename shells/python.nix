b25aecc01f4e3e322bbd4d06853d53d4efae9b6d
Christopher James Hayward <chris@chrishayward.xyz>
2022-03-10 14:02:01 -0500

    Replace pyls with python-lsp

--- shells/python.nix
+++ shells/python.nix
@@ -9,9 +9,6 @@ mkShell {
     python310Packages.python-lsp-black
     python310Packages.python-lsp-server
     python310Packages.python-lsp-jsonrpc
-    # python310Packages.pyls-mypy
-    # python310Packages.pyls-isort
-    # python310Packages.pyls-black
   ];
   shellHook = ''
   '';
