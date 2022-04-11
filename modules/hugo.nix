61b3f31fa7f0141e2e3c328adbe19ca6a0aec1a8
Christopher James Hayward <chris@chrishayward.xyz>
2022-04-11 19:35:22 -0400

    Fix hugo script

--- modules/hugo.nix
+++ modules/hugo.nix
@@ -3,7 +3,7 @@
 
 let
   mySiteDir = "/etc/dotfiles/docs/public/";
-  mySiteTgt = "unbuntu@chrishayward.xyz:/var/www/wedding";
+  mySiteTgt = "ubuntu@chrishayward.xyz:/var/www/wedding";
   mySiteUpdate = pkgs.writeShellScriptBin "my-site-update" ''
     ${pkgs.rsync}/bin/rsync -aP ${mySiteDir} ${mySiteTgt}
   '';
