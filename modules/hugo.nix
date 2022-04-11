88f7ea51af5b1cf6302f0dd81995b4fd9cb1eec5
Christopher James Hayward <chris@chrishayward.xyz>
2022-04-11 19:45:04 -0400

    Add build script to hugo module

--- modules/hugo.nix
+++ modules/hugo.nix
@@ -4,13 +4,16 @@
 let
   mySiteDir = "/etc/dotfiles/docs/public/";
   mySiteTgt = "ubuntu@chrishayward.xyz:/var/www/wedding";
+  mySiteBuild = pkgs.writeShellScriptBin "my-site-build" ''
+    pushd ${mySiteDir}../ && ${pkgs.hugo}/bin/hugo -v && popd
+  '';
   mySiteUpdate = pkgs.writeShellScriptBin "my-site-update" ''
     ${pkgs.rsync}/bin/rsync -aP ${mySiteDir} ${mySiteTgt}
   '';
 
 in {
   environment.systemPackages = [
-    pkgs.hugo
+    mySiteBuild
     mySiteUpdate
   ];
 }
