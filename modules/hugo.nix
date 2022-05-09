e4d1b62be6dc314323f305a2745bba61efd02261
Christopher James Hayward <chris@chrishayward.xyz>
2022-05-09 10:52:48 -0400

    Silence pushd/popd

--- modules/hugo.nix
+++ modules/hugo.nix
@@ -5,7 +5,9 @@ let
   mySiteDir = "/etc/dotfiles/docs/public/";
   mySiteTgt = "ubuntu@chrishayward.xyz:/var/www/wedding";
   mySiteBuild = pkgs.writeShellScriptBin "my-site-build" ''
-    pushd ${mySiteDir}../ && ${pkgs.hugo}/bin/hugo -v ; popd
+    pushd ${mySiteDir}../ > /dev/null &&
+    ${pkgs.hugo}/bin/hugo -v ;
+    popd > /dev/null
   '';
   mySiteUpdate = pkgs.writeShellScriptBin "my-site-update" ''
     ${pkgs.rsync}/bin/rsync -aP ${mySiteDir} ${mySiteTgt}
